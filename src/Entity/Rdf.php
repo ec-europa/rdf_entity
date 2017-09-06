a2c4b4520e4b757c8a83562675bda45135ca220c
Pieter Frenssen <pieter@frenssen.be>
2017-09-06 12:00:20 +0300

    Update documentation.

--- src/Entity/Rdf.php
+++ src/Entity/Rdf.php
@@ -12,63 +12,9 @@ use Drupal\Core\Entity\EntityChangedTrait;
 use Drupal\user\UserInterface;
 
 /**
- * Defines the ContentEntityExample entity.
+ * Defines the RDF entity.
  *
  * @ingroup rdf_entity
- *
- * This is the main definition of the entity type. From it, an entityType is
- * derived. The most important properties in this example are listed below.
- *
- * id: The unique identifier of this entityType. It follows the pattern
- * 'moduleName_xyz' to avoid naming conflicts.
- *
- * label: Human readable name of the entity type.
- *
- * handlers: Handler classes are used for different tasks. You can use
- * standard handlers provided by D8 or build your own, most probably derived
- * from the standard class. In detail:
- *
- * - view_builder: we use the standard controller to view an instance. It is
- *   called when a route lists an '_entity_view' default for the entityType
- *   (see routing.yml for details. The view can be manipulated by using the
- *   standard drupal tools in the settings.
- *
- * - list_builder: We derive our own list builder class from the
- *   entityListBuilder to control the presentation.
- *   If there is a view available for this entity from the views module, it
- *   overrides the list builder. @todo: any view? naming convention?
- *
- * - form: We derive our own forms to add functionality like additional fields,
- *   redirects etc. These forms are called when the routing list an
- *   '_entity_form' default for the entityType. Depending on the suffix
- *   (.add/.edit/.delete) in the route, the correct form is called.
- *
- * - access: Our own accessController where we determine access rights based on
- *   permissions.
- *
- * More properties:
- *
- *  - base_table: Define the name of the table used to store the data. Make sure
- *    it is unique. The schema is automatically determined from the
- *    BaseFieldDefinitions below. The table is automatically created during
- *    installation.
- *
- *  - fieldable: Can additional fields be added to the entity via the GUI?
- *    Analog to content types.
- *
- *  - entity_keys: How to access the fields. Analog to 'nid' or 'uid'.
- *
- *  - links: Provide links to do standard tasks. The 'edit-form' and
- *    'delete-form' links are added to the list built by the
- *    entityListController. They will show up as action buttons in an additional
- *    column.
- *
- * There are many more properties to be used in an entity type definition. For
- * a complete overview, please refer to the '\Drupal\Core\Entity\EntityType'
- * class definition.
- *
- * The following construct is the actual definition of the entity type which
- * is read and cached. Don't forget to clear cache after changes.
  *
  * @ContentEntityType(
  *   id = "rdf_entity",
@@ -109,33 +55,6 @@ use Drupal\user\UserInterface;
  *   permission_granularity = "bundle",
  *   common_reference_target = TRUE,
  * )
- *
- * The 'links' above are defined by their path. For core to find the
- * corresponding route, the route name must follow the correct pattern:
- *
- * entity.<entity-name>.<link-name> (replace dashes with underscores)
- * Example: 'entity.rdf_entity.canonical'
- *
- * See routing file above for the corresponding implementation
- *
- * The Rdf class defines methods and fields for the Rdf entity.
- *
- * Being derived from the ContentEntityBase class, we can override the methods
- * we want. In our case we want to provide access to the standard fields about
- * creation and changed time stamps.
- *
- * Our interface (see RdfInterface) also exposes the EntityOwnerInterface.
- * This allows us to provide methods for setting and providing ownership
- * information.
- *
- * The most important part is the definitions of the field properties for this
- * entity type. These are of the same type as fields added through the GUI, but
- * they can by changed in code. In the definition we can define if the user with
- * the rights privileges can influence the presentation (view, edit) of each
- * field.
- *
- * The class also uses the EntityChangedTrait trait which allows it to record
- * timestamps of save operations.
  */
 class Rdf extends ContentEntityBase implements RdfInterface {
 
