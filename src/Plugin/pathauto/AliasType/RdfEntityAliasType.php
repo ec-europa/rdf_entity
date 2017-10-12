2cfd3d48dda7aeead5f48608e51b0a2c5e3ab0d7
Pieter Frenssen <pieter@frenssen.be>
2017-10-12 14:15:24 +0200

    Allow to update existing URL aliases, and regenerate URL aliases for all paths.

--- src/Plugin/pathauto/AliasType/RdfEntityAliasType.php
+++ src/Plugin/pathauto/AliasType/RdfEntityAliasType.php
@@ -6,6 +6,7 @@ namespace Drupal\rdf_entity\Plugin\pathauto\AliasType;
 
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\pathauto\Plugin\pathauto\AliasType\EntityAliasTypeBase;
+use Drupal\rdf_entity\Entity\Query\Sparql\Query;
 use Drupal\rdf_entity\UriEncoder;
 
 /**
@@ -45,37 +46,36 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
       $context['sandbox']['count'] = 0;
     }
 
+    $query = $this->getRdfEntityQuery();
+    $query->addTag('rdf_entity_pathauto_bulk_update');
+
     switch ($action) {
       case 'create':
-        // Get a list of all source paths that start with '/rdf_entity/'.
-        $source_paths = $this->getSourcePaths($context['sandbox']);
-        // Convert the source paths to entity IDs.
-        $aliased_rdf_entity_ids = $this->convertPathsToEntityIds($source_paths);
-        // Get a list of 25 RDF entity IDS that are not in the list of URL
-        // aliases.
-        $query = $this->getRdfEntityQuery();
+        // Process RDF entities that are not in the list of URL aliases.
+        $aliased_rdf_entity_ids = $this->getAliasedEntityIds($context['sandbox']);
         if (!empty($aliased_rdf_entity_ids)) {
           $query->condition('id', $aliased_rdf_entity_ids, 'NOT IN');
         }
         break;
+
       case 'update':
-        // Get a list of 25 RDF entity IDs from the `url_alias` table with a
-        // `source` field that starts with `rdf_entity/`.
-        $query = $this->database->select('url_alias', 'ua');
+        // Process RDF entities that are in the list of URL aliases.
+        $aliased_rdf_entity_ids = $this->getAliasedEntityIds($context['sandbox']);
+        if (!empty($aliased_rdf_entity_ids)) {
+          $query->condition('id', $aliased_rdf_entity_ids, 'IN');
+        }
         break;
+
       case 'all':
-        $rdf_entity_ids = $this->getRdfEntityIds($context['sandbox']);
-        $query = $this->database->select('url_alias', 'ua');
-        // Nothing to do. We want all paths.
+        // Nothing to filter. We want all entities.
         break;
+
       default:
         // Unknown action. Abort!
         return;
     }
 
-    $query->addTag('rdf_entity_pathauto_bulk_update');
-
-    // Get the total amount of items to process.
+    // Keep track of the total amount of items to process.
     if (!isset($context['sandbox']['total'])) {
       $count_query = clone $query;
       $context['sandbox']['total'] = $count_query->count()->execute();
@@ -88,12 +88,10 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
     }
 
     $query->range($context['sandbox']['count'], 25);
-    // $ids = $query->execute()->fetchCol();
     $ids = $query->execute();
 
     $updates = $this->bulkUpdate($ids);
     $context['sandbox']['count'] += count($ids);
-    // $context['sandbox']['current'] = max($ids);
     $context['results']['updates'] += $updates;
     $context['message'] = $this->t('Updated alias for Rdf entity @id.', array('@id' => end($ids)));
 
@@ -113,7 +111,7 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
    * @return array
    *   An array of RDF entity IDs.
    */
-  protected function getRdfEntityIds(array $sandbox) : array {
+  protected function getRdfEntityIds(array &$sandbox) : array {
     if (empty($sandbox['rdf_entity_ids'])) {
       $sandbox['rdf_entity_ids'] = $this->getRdfEntityQuery()->execute();
     }
@@ -127,14 +125,11 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
    * @return \Drupal\rdf_entity\Entity\Query\Sparql\Query
    *   The entity query.
    */
-  protected function getRdfEntityQuery() {
+  protected function getRdfEntityQuery() : Query {
     /** @var \Drupal\rdf_entity\Entity\RdfEntitySparqlStorage $storage */
     $storage = $this->entityTypeManager->getStorage('rdf_entity');
     /** @var \Drupal\rdf_entity\Entity\Query\Sparql\Query $query */
     $query = $storage->getQuery();
-    // @todo Is it OK to only return the published entities? Unpublished
-    //   entities don't need to get URL aliased, but what happens if an
-    //   unpublished entity gets published? Does it get an alias at that point?
     $query->setGraphType($storage->getGraphHandler()->getEntityTypeEnabledGraphs());
 
     return $query;
@@ -151,8 +146,8 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
    * @return array
    *   An associative array of source paths, keyed by url alias ID.
    */
-  protected function getSourcePaths(array $sandbox) : array {
-    if (empty($sandbox['source_paths'])) {
+  protected function getSourcePaths(array &$sandbox) : array {
+    if (!isset($sandbox['source_paths'])) {
       $query = $this->database->select('url_alias', 'ua');
       $query->fields('ua', ['pid', 'source']);
       $query->condition('source', '/rdf_entity/%', 'LIKE');
@@ -183,6 +178,26 @@ class RdfEntityAliasType extends EntityAliasTypeBase implements ContainerFactory
       // Strip off the leading '/rdf_entity/' from the path and decode it.
       return UriEncoder::decodeUrl(substr($source_path, 12));
     }, $source_paths);
+  }
+
+  /**
+   * Returns a list of RDF entity IDs that have a URL alias.
+   *
+   * This result will be persisted in the batch operation sandbox.
+   *
+   * @param array $sandbox
+   *   The batch operation sandbox.
+   *
+   * @return array
+   *   An array of RDF entity IDs.
+   */
+  protected function getAliasedEntityIds(array &$sandbox) : array {
+    // Get a list of all source paths that start with '/rdf_entity/' from the
+    // URL alias table.
+    $source_paths = $this->getSourcePaths($sandbox);
+
+    // Convert the source paths to entity IDs.
+    return $this->convertPathsToEntityIds($source_paths);
   }
 
   /**
