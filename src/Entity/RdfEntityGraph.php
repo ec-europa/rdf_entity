ae40916388e3a56589a622e7440fe6b041cdbbb1
Claudiu Cristea <clau.cristea@gmail.com>
2018-01-10 16:35:29 +0200

    Add setters/getters and a new property 'entity_types' to RdfEntityGraph.

--- src/Entity/RdfEntityGraph.php
+++ src/Entity/RdfEntityGraph.php
@@ -4,6 +4,9 @@ namespace Drupal\rdf_entity\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
 use Drupal\Core\Entity\Annotation\ConfigEntityType;
+use Drupal\Core\Entity\ContentEntityTypeInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\rdf_entity\RdfEntityGraphInterface;
 
 /**
@@ -24,6 +27,7 @@ use Drupal\rdf_entity\RdfEntityGraphInterface;
  *     "id",
  *     "name",
  *     "description",
+ *     "entity_types",
  *   },
  * )
  */
@@ -49,5 +53,89 @@ class RdfEntityGraph extends ConfigEntityBase implements RdfEntityGraphInterface
    * @var string
    */
   protected $description;
+
+  /**
+   * Entity type IDs where this graph applies.
+   *
+   * NULL means it applies to all entity types.
+   *
+   * @var string[]|null
+   */
+  protected $entity_types = NULL;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setName(string $name): RdfEntityGraphInterface {
+    $this->name = $name;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setDescription(string $description): RdfEntityGraphInterface {
+    $this->description = $description;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription(): string {
+    return $this->description;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityTypeIds(): ?array {
+    return $this->entity_types ?: NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setEntityTypeIds(?array $entity_type_ids): RdfEntityGraphInterface {
+    if (empty($entity_type_ids)) {
+      $this->entity_types = NULL;
+    }
+    else {
+      foreach ($entity_type_ids as $entity_type_id) {
+        if (!$this->entityTypeManager()->getDefinition($entity_type_id, FALSE)) {
+          throw new \InvalidArgumentException("Invalid entity type: '$entity_type_id'.");
+        }
+        $storage = $this->entityTypeManager()->getStorage($entity_type_id);
+        if (!$storage instanceof RdfEntitySparqlStorage) {
+          throw new \InvalidArgumentException("Entity type '$entity_type_id' not a RDF entity.");
+        }
+      }
+      $this->entity_types = $entity_type_ids;
+    }
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function delete() {
+    if ($this->id() === static::DEFAULT) {
+      throw new \RuntimeException("The '" . static::DEFAULT . "' graph cannot be deleted.");
+    }
+    parent::delete();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(EntityStorageInterface $storage) {
+    parent::preSave($storage);
+    if ($this->id() === static::DEFAULT) {
+      if (!$this->status()) {
+        throw new \RuntimeException("The '" . static::DEFAULT . "' graph cannot be disabled.");
+      }
+    }
+  }
 
 }
--- src/RdfEntityGraphInterface.php
+++ src/RdfEntityGraphInterface.php
@@ -18,4 +18,59 @@ interface RdfEntityGraphInterface extends ConfigEntityInterface {
    */
   const DEFAULT = 'default';
 
+  /**
+   * Set the graph name.
+   *
+   * @param string $name
+   *   The graph name.
+   *
+   * @return $this
+   */
+  public function setName(string $name): self;
+
+  /**
+   * Set the graph description.
+   *
+   * @param string $description
+   *   The graph description.
+   *
+   * @return $this
+   */
+  public function setDescription(string $description): self;
+
+  /**
+   * Gets the graph description.
+   *
+   * @return string
+   */
+  public function getDescription(): string;
+
+  /**
+   * Gets the entity types supporting this graph.
+   *
+   * @return string[]|null
+   *   A list of entity type IDs or NULL if this graph is available to all
+   *   entity types.
+   */
+  public function getEntityTypeIds(): ?array;
+
+  /**
+   * Sets the entity type IDs to whom this graph is made available.
+   *
+   * @param string[]|null $entity_type_ids
+   *   A list of entity type IDs or NULL to expose this graph to all types.
+   *
+   * @return $this
+   *
+   * @throws \InvalidArgumentException
+   *   If there are non-eligible entity types in the list. Eligible entity type
+   *   IDs are those each fulfilling all the following conditions:
+   *   - An entity type exists for that ID,
+   *   - The entity type is a content entity type,
+   *   - The entity type storage is an instance of RdfEntitySparqlStorage.
+   *
+   * @see \Drupal\rdf_entity\Entity\RdfEntitySparqlStorage
+   */
+  public function setEntityTypeIds(?array $entity_type_ids): self;
+
 }
