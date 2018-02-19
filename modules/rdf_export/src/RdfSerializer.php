e35303fda4f27264dabbc76a99741a9dfa9ef5ef
Francesco Sardara <francesco.sardara@one-agency.be>
2018-02-19 17:01:47 +0100

    Issue #ISAICP-4216: extract existing code to a serializer service.

---
+++ modules/rdf_export/src/RdfSerializer.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\rdf_export;
+
+use Drupal\rdf_entity\Database\Driver\sparql\Connection;
+use Drupal\rdf_entity\RdfInterface;
+
+/**
+ * Service to serialise RDF entities into various formats.
+ */
+class RdfSerializer implements RdfSerializerInterface {
+
+  /**
+   * The Sparql connection object.
+   *
+   * @var \Drupal\rdf_entity\Database\Driver\sparql\Connection
+   */
+  protected $sparqlEndpoint;
+
+  /**
+   * Instantiates a new RdfSerializer object.
+   *
+   * @param \Drupal\rdf_entity\Database\Driver\sparql\Connection $sparqlEndpoint
+   *   The Sparql connection object.
+   */
+  public function __construct(Connection $sparqlEndpoint) {
+    $this->sparqlEndpoint = $sparqlEndpoint;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function serializeEntity(RdfInterface $entity, string $format = 'turtle'): string {
+    $query = <<<SPARQL
+CONSTRUCT {
+  ?s ?p ?o .
+}
+WHERE {
+  {
+    ?s ?p ?o .
+    VALUES ?s { <{$entity->id()}> }
+  } UNION {
+     ?s ?p ?o .
+     VALUES ?o { <{$entity->id()}> }
+  }
+}
+SPARQL;
+
+    /** @var \EasyRdf\Graph $graph */
+    $graph = $this->sparqlEndpoint->query($query);
+    return $graph->serialise($format);
+  }
+
+}
