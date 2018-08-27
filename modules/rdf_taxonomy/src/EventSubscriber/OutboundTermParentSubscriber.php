2ea947439ebcf11f856cd5e70599b0b4852d93e3
Claudiu Cristea <clau.cristea@gmail.com>
2018-08-27 17:21:08 +0300

    Fix the term parent type.
    Fix the term parent type.

---
+++ modules/rdf_taxonomy/src/EventSubscriber/OutboundTermParentSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\rdf_taxonomy\EventSubscriber;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\rdf_entity\Event\OutboundValueEvent;
+use Drupal\rdf_entity\Event\RdfEntityEvents;
+use function GuzzleHttp\Psr7\str;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Massages outbound date/time values.
+ */
+class OutboundTermParentSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      RdfEntityEvents::OUTBOUND_VALUE => 'fixParentTermId',
+    ];
+  }
+
+  /**
+   * Fixes the term parent ID.
+   *
+   * Drupal core uses taxonomy terms with numeric IDs. If case, we convert the
+   * term ID, from a numeric type to string.
+   *
+   * @param \Drupal\rdf_entity\Event\OutboundValueEvent $event
+   *   The outbound value event.
+   */
+  public function fixParentTermId(OutboundValueEvent $event) {
+    if ($event->getEntityTypeId() === 'taxonomy_term' && $event->getField() === 'parent') {
+      $event->setValue((string) $event->getValue());
+    }
+  }
+
+}
