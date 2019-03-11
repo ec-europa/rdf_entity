63561212e6ec29f562598188f0e6158b6bc7ce10
Sander Van Dooren <sander.vandooren@gmail.com>
2019-03-11 09:22:35 +0100

    Get graph handler from storage.

--- src/SparqlResultFilter.php
+++ src/SparqlResultFilter.php
@@ -2,12 +2,14 @@
 
 namespace Drupal\rdf_entity;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 class SparqlResultFilter {
 
-  protected $graphHandler;
+  protected $entityTypeManager;
 
-  public function __construct(RdfGraphHandlerInterface $graph_handler) {
-    $this->graphHandler = $graph_handler;
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   public function filter(RawEntityRepository $entity_repo, $graph_priorities, $entity_type_id) : RawEntityRepository {
@@ -22,7 +24,11 @@ class SparqlResultFilter {
   }
 
   protected function graphUriFromPrio(string $prio, $entity_type_id) {
-    $graph_uris = $this->graphHandler->getEntityTypeGraphUris($entity_type_id);
+    $storage = $this->entityTypeManager->getStorage($entity_type_id);
+    if (!$storage instanceof RdfEntitySparqlStorageInterface) {
+      throw new \Exception('Storage must implement RDF Storage interface.');
+    }
+    $graph_uris = $storage->getGraphHandler()->getEntityTypeGraphUris($entity_type_id);
     return array_column($graph_uris, $prio);
   }
 }
